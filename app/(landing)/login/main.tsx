9d37e365c023020b65bff0d7ee72a7a3c7f94799
Advay <80443087+ady00@users.noreply.github.com>
2024-01-26 06:07:13 -0800

    Update main.tsx

--- app/(landing)/login/main.tsx
+++ app/(landing)/login/main.tsx
@@ -14,7 +14,7 @@ const getURL = () => {
     process.env.NEXT_PUBLIC_LIVE_DOMAIN ??
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'https://nyt-plus.vercel.app/
+    'https://nyt-plus.vercel.app/'
   // 'http://local.qwerty.boats:3000/'
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`
